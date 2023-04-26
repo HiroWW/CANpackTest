ff085ae40b735bee2cd79c3ae4294a4ab7f16fb5
hiroaki <hiroaki.tokyu8500@gmail.com>
2023-04-26 17:35:04 +0900

    fix:sendいれてコンパイル通した

--- src/global.hpp
+++ src/global.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include "CommunicationPacks.hpp"
+#include "../.pio/libdeps/teensy41/CommunicationPacks/src/CommunicationPacks.hpp"
 
 extern Tail2Master mp;
 extern Tail2Log lp;
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
 #include <Arduino.h>
 #include "global.hpp"
+#include "CANpack.hpp"
 void setup() {
   // put your setup code here, to run once:
-  //IXEI
+  CANpack0.CANsetup();
 }
 
+int i = 0;
 void loop() {
   // put your main code here, to run repeatedly:
+  //受信をためす。
+  for (int j = 0; j < 4; j++) {
+    mp.gyro[j] = (j + 1) * 10;
+  }
+  mp.apflag = i;
+  i++;
+  int ID;
+  ID = 0;
+  CANpack0.send(ID,&mp);
 }

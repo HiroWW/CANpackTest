3b756686963c05831193d1959573527665522efb
hiroaki <hiroaki.tokyu8500@gmail.com>
2023-06-12 15:21:09 +0900

    sendを実装

--- src/main.cpp
+++ src/main.cpp
@@ -6,39 +6,39 @@ void setup() {
   CANpack0.CANsetup();
 }
 
-int i = 0;
-int loopCount = 0;
 void loop() {
-  // put your main code here, to run repeatedly:
+  // 送信か受信のどちらかをコメントアウトする
+
+  // 二つのpackの中身を定義してCANバスに送信する
+
+  // setup Master to Interface pack
+  mip.attitude_dt = 11.1f;
+  mip.main_dt = 22.2f;
+  mip.control_dt = 33.3f;
+  // send to can bus
+  CANpack0.send(0,&mip);
   
-  //受信
-  // for (int j = 0; j < 4; j++) {
-  //   mp.gyro[j] = (j + 1) * 10;
-  // }
-  // mp.apflag = i;
-
-  // for (int j = 0; j < 3; j++) {
-  //   lp.gyro[j] = (j + 1) * 10;
-  // }
-  // lp.acc = 321.0f;
-  // lp.apflag = i;
-  // i++;
-  // int ID;
-  // ID = 1;
-  // CANpack0.send(ID,&lp);
-
-  can0.events();
-  if (loopCount % 10000 == 0){
-    Serial.println("Master To IF ");
-    Serial.print("attituded_dt = "); Serial.println(mip.attitude_dt);
-    Serial.print("main_dt = "); Serial.println(mip.main_dt);
-    Serial.print("control_dt = "); Serial.println(mip.control_dt);
-    Serial.println("IF To Master ");
-    Serial.print("strain[0] = "); Serial.println(imp.strain[0]);
-    Serial.print("strain[1] = "); Serial.println(imp.strain[1]);
-    Serial.print("strain[2] = "); Serial.println(imp.strain[2]);
-    Serial.print("strain[3] = "); Serial.println(imp.strain[3]);
+  //setup Interface to Master pack
+  for (int i = 0; i < 4 ; i++ ){
+    imp.strain[i] = (i + 1) * 100.0f;
   }
-  loopCount++ ;
+  // send to can bus
+  CANpack0.send(1,&imp);
+
+  // 割り込みで受信して内容をシリアルモニタに表示
+
+  // can0.events();
+  // if (loopCount % 10000 == 0){
+  //   Serial.println("Master To IF ");
+  //   Serial.print("attituded_dt = "); Serial.println(mip.attitude_dt);
+  //   Serial.print("main_dt = "); Serial.println(mip.main_dt);
+  //   Serial.print("control_dt = "); Serial.println(mip.control_dt);
+  //   Serial.println("IF To Master ");
+  //   Serial.print("strain[0] = "); Serial.println(imp.strain[0]);
+  //   Serial.print("strain[1] = "); Serial.println(imp.strain[1]);
+  //   Serial.print("strain[2] = "); Serial.println(imp.strain[2]);
+  //   Serial.print("strain[3] = "); Serial.println(imp.strain[3]);
+  // }
+  // loopCount++ ;
 
 }

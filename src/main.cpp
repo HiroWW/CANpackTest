42dcde03d86f35ca9cc78a821d5177b0496fc6e1
hiroaki <hiroaki.tokyu8500@gmail.com>
2023-06-23 23:19:52 +0900

    fix : いまうごいてます

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@ void setup() {
 }
 
 int i = 0;
-int loopCount = 0;
 float beforeStrain = 1.0f;
 void loop() {
   // put your main code here, to run repeatedly:
@@ -34,26 +33,11 @@ void loop() {
   // CANpack0.send(ID,&lp);
 
   can0.events();
-  //if (Serial.availableForWrite() > 0){
-  // if (loopCount % 100000 == 0){
-  //   Serial.println("Master To IF sending content");
-  //   Serial.print("attituded_dt = "); Serial.println(mip.attitude_dt);
-  //   Serial.print("main_dt = "); Serial.println(mip.main_dt);
-  //   Serial.print("control_dt = "); Serial.println(mip.control_dt);
-  //   Serial.println("IF To Master sending content");
-  //   Serial.print("strain[0] = "); Serial.println(imp.strain[0]);
-  //   Serial.print("strain[1] = "); Serial.println(imp.strain[1]);
-  //   Serial.print("strain[2] = "); Serial.println(imp.strain[2]);
-  //   Serial.print("strain[3] = "); Serial.println(imp.strain[3]);
-  //   Serial.print("strain[4] = "); Serial.println(imp.strain[4]);
-  //   Serial.println("Master to Tail content");
-  //   Serial.print("updateTime"); Serial.println(mtp.updateTime);
-  //   Serial.print("drCommand"); Serial.println(mtp.drCommand);
-  //   Serial.print("deCommand"); Serial.println(mtp.deCommand);
-  //   // Serial.println(mtp.err_state[1]);
-  //   // Serial.println(mtp.gravity[2]);
-  //   Serial.print("mtp.mode"); Serial.println(mtp.mode);
+  // for (int i = 0; i < 5; i++) {
+  //   CANpack0.packPointer[i] = p[i];
+  //   // p[i] = CANpack0.packPointer[i];
   // }
+
   UTHAPS::println("Master To IF sending content");
   UTHAPS::println("attituded_dt = ",mip.attitude_dt);
   UTHAPS::println("main_dt = ", mip.main_dt);
@@ -68,23 +52,8 @@ void loop() {
   UTHAPS::println("updateTime = ", mtp.updateTime);
   UTHAPS::println("drCommand = ", mtp.drCommand);
   UTHAPS::println("deCommand = ", mtp.deCommand);
-  // UTHAPS::println(mtp.err_state[1]);
-  // UTHAPS::println(mtp.gravity[2]);
+  UTHAPS::println("err state = ", mtp.err_state[1]);
+  UTHAPS::println("gravity = ", mtp.gravity[2]);
   UTHAPS::println("mtp.mode = ", mtp.mode);
-
-  // Check when receiving the data fails
-  // UTHAPS::println(loopCount);
-  // if (loopCount > 50000){
-  //   if (imp.strain[0] == 0.0f && beforeStrain == 0.0f){
-  //     delay(10000);
-  //   }   
-  // }
-  // beforeStrain = imp.strain[0];
-
-  //Reset If to Master pack content
-  // for (int i = 0; i < 5; i++){
-  //   imp.strain[i] = 0.0f;
-  // }
-  loopCount++ ;
 
 }

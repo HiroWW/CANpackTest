035f845b93799eaa7aaee1e5136291835880c242
hiroaki <hiroaki.tokyu8500@gmail.com>
2023-07-28 11:22:25 +0900

    debug : main単体ならとりあえずreadまでは通る、コンパイルが。

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
-#include "global.hpp"
-#include "CANpack.hpp"
+#include <FlexCAN_T4.h>
+#include <TeensyCAN.h>
 #include "print.hpp"
+#include "CommunicationPacks.hpp"
 
 //--------------------------------------
 //   change here to switch read/send
@@ -9,11 +10,46 @@
 bool IFREAD = true;
 //--------------------------------------
 
+FlexCAN_T4FD<CAN3, RX_SIZE_256, TX_SIZE_16> can0;
+TeensyCAN node36 = TeensyCAN(36);
+
+CAN::MasterToIF mip;
+CAN::IFToMaster imp;
+CAN::MasterToTail mtp;
+CAN::TailToMaster tmp;
+CAN::TailToIF tip;
+
+CAN::pack* p[5] = {&mip,&imp,&mtp,&tmp,&tip};
+int len[5] = {sizeof(mip), sizeof(imp),sizeof(mtp), sizeof(tmp), sizeof(tip)};
+
+void CANread(const uint8_t* buffer, uint16_t length, AsyncTC info) {
+  int ID_read;
+  ID_read = info.packetid;
+  memcpy(p[ID_read], buffer, len[ID_read]);
+}
 
 void setup() {
   delay(3000);
   Serial.println("Waiting for setup...");
-  CANpack0.CANsetup();
+
+  can0.begin();
+
+  Node.setBus(_CAN3);
+  Node.setID(36);
+  Node.onReceive(CANread);
+
+  CANFD_timings_t config;
+  config.clock = CLK_24MHz;
+  config.baudrate = 1000000;
+  config.baudrateFD = 2000000;
+  config.propdelay = 190;
+  config.bus_length = 1;
+  config.sample = 87.5;
+  can0.setRegions(64);
+  can0.setBaudRate(config);
+  can0.enableMBInterrupts();
+  can0.mailboxStatus();
+
   Serial.println("CAN setup : COMPLETE");
 }
 
@@ -21,6 +57,7 @@ int loopCount = 0;
 float beforeStrain = 1.0f;
 void loop() {
   can0.events();
+  Node.events();
   if (IFREAD){
     // case read
     // for (int i = 0; i < 5; i++) {
@@ -44,7 +81,7 @@ void loop() {
     mip.attitude_dt = 11.1f * (loopCount % 5 + 1);
     mip.main_dt = 22.2f * (loopCount % 5 + 1);
     mip.control_dt = 33.3f * (loopCount % 5 + 1);
-    CANpack0.send(0,&mip);
+    // CANpack0.send(0,&mip);
     Serial.println("Master to IF sending success");
   } else {
 
@@ -54,7 +91,7 @@ void loop() {
     for (int i = 0; i < 5 ; i++ ){
       imp.strain[i] = (i + 1) * 11.1f * (loopCount % 5 + 1);
     }
-    CANpack0.send(1,&imp);
+    // CANpack0.send(1,&imp);
     Serial.println("IF to Master : send SUCCESS");
 
     // setup Master to Tail pack and send it to CAN bus
@@ -68,7 +105,7 @@ void loop() {
     mtp.gravity[1] = 66.8f;
     mtp.gravity[2] = 99.4f;
     mtp.mode = 2.0f * (loopCount % 5 + 1);
-    CANpack0.send(2,&mtp);
+    // CANpack0.send(2,&mtp);
     Serial.println("Master to Tail : send SUCCESS");
     UTHAPS::println("Master To IF sending content");
     UTHAPS::println("attituded_dt = ",mip.attitude_dt);
